d5ad5d4a310aa98068883288905b9e01d8289301
apkuki <98207992+apkuki@users.noreply.github.com>
2024-08-28 15:44:31 +0200

    Update tableview.user.js
    Updated with of Table view 280px

--- tableview.user.js
+++ tableview.user.js
@@ -107,7 +107,7 @@
     GM_addStyle(`
         @media (min-width: 768px) {
             #calendar .provider-column {
-                width: 350px !important;
+                width: 280px !important;
             }
         }
     `);
